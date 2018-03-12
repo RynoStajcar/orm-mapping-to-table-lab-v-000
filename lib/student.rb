b200cba4428db73a912085911197e8f728e7b0c3
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-12 15:30:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,4 +20,11 @@ class Student
       DB[:conn].execute(sql)
     end
 
+    def self.drop_table
+      sql = <<-SQL
+        DROP TABLE self
+        SQL
+        DB[:conn].execute(sql)
+      end
+
 end
