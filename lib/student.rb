7126b38e92a3af8f3415c873ace0c7015d0ce4b9
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-12 15:16:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,4 +2,16 @@ class Student
 
   attr_accessor :name, :grade
   attr_reader :id
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+      DB[:conn].execute(sql)
+    end
+
 end
